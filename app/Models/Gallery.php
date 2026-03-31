2277edbdf12162cbb7a4b958bffd8fd6e4a10921
ilhamsaputra <ilhamsaputra@ptskk.id>
2026-03-31 11:54:53 +0700

    fix bug

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\HasTranslation;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class Gallery extends Model 
 {
-    use HasUuids;
+    use HasUuids, HasTranslation;
 
     protected $keyType = 'string';
     public $incrementing = false;

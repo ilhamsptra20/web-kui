a02aa622bbb9ec1180e755668c5f09645b855f73
ilhamsaputra <ilhamsaputra@ptskk.id>
2026-04-13 19:18:42 +0700

    update

---
+++ resources/views/pages/marketing/page.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.marketing')
+
+@section('content')
+
+{{-- =============================================
+   BREADCRUMB
+============================================= --}}
+<div class="breadcrumb-area bg-f round-20 position-relative z-1">
+    <div class="container text-center">
+
+        <ul class="br-menu text-center bg_secondary d-inline-block list-unstyled mb-15">
+            <li class="position-relative fs-13 fw-semibold ls-1 d-inline-block">
+                <a href="{{ url('/') }}">HOME</a>
+            </li>
+            <li class="position-relative fs-13 fw-semibold ls-1 d-inline-block">
+                {{ __('nav.page') }}
+            </li>
+            <li class="position-relative fs-13 fw-semibold ls-1 d-inline-block">
+                {{ $page->title ?? 'Page' }}
+            </li>
+        </ul>
+
+        <h2 class="section-title style-one fw-medium font-secondary text-black text-center mb-6">
+            {{ $breadcrumb['title'] ?? 'Galeri Kegiatan' }}
+        </h2>
+
+    </div>
+</div>
+
+@endsection
